72d031423479466fc04ceda7b0894acec9124e7c
carlitrosgp14 <carlosegp.2496@gmail.com>
2025-10-17 09:52:06 -0300

    ongoing work in flex layour ServicePage
    ongoing work in flex layour ServicePage
    integrated 3 flex containers for three new cards.

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -5,7 +5,9 @@ function ServicesPage() {
   return (
     <div className="service-page-container">
       <h1>Servicios</h1>
-      <div className="main-flex-container">
+      {/* Flex container 1*/}
+      <div className="flex-container">
+        {/* First card container */}
         <div className="card-flex-container">
           <img src="/images/lic-martinez-logo.png" alt="logo" />
           <div className="card-service-text-c">
@@ -13,13 +15,58 @@ function ServicesPage() {
             <p className="card-service-p"> TESTING </p>
             <link rel="stylesheet" href="/contacto" className="href" />
           </div>
-          <div className="card-flex-container">
-            <img src="/images/lic-martinez-logo.png" alt="logo" />
-            <div className="card-service-text-c">
-              <h3 className="card-subheading">SERVICE 1</h3>
-              <p className="card-service-p"> TESTING </p>
-              <link rel="stylesheet" href="/contacto" className="href" />
-            </div>
+        </div>
+        {/* Second Card Container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+      </div>
+
+      {/* Flex container 2*/}
+      <div className="flex-container">
+        {/* Third card container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+        {/* Fourth Card Container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+      </div>
+
+      {/* Flex container 3*/}
+      <div className="flex-container">
+        {/* Third card container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+        {/* Fourth Card Container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
           </div>
         </div>
       </div>
