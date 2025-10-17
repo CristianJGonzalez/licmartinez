5126fdaf24fffdaecb9fdb623e15674c43a021de
carlitrosgp14 <carlosegp.2496@gmail.com>
2025-10-16 23:14:06 -0300

    ongoin work in ServicePage
    modifying react component ServicePage with a flex approach

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import "./ServicePage.css";
 
-function ServicesPage({ services }) {
+function ServicesPage() {
   return (
-    <div className="service-page ">
+    <div className="service-page-container">
       <h1>Servicios</h1>
-      <ul>
-        {services.map((s) => (
-          <li key={s.id}>
-            <h2>{s.name}</h2>
-            <p>{s.description}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="main-flex-container">
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+          <div className="card-flex-container">
+            <img src="/images/lic-martinez-logo.png" alt="logo" />
+            <div className="card-service-text-c">
+              <h3 className="card-subheading">SERVICE 1</h3>
+              <p className="card-service-p"> TESTING </p>
+              <link rel="stylesheet" href="/contacto" className="href" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
