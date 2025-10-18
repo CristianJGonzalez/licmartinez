688a6113bd7f2fcf9ceda05f1ef20f427344bbe6
carlitrosgp14 <carlosegp.2496@gmail.com>
2025-10-18 11:28:34 -0300

    completed flex layout in Service Page
    Need to add content, requested content to content writer. Waiting for it .

---
+++ src/Components/indexAllComponents.js
@@ -0,0 +1,12 @@
+// In this JS file we are mapping all the compones to import them in the componen tree aka App.jsx
+// in 1 single line
+
+export { default as serviceData } from "./serviceData";
+export { default as Nav } from "./Nav";
+export { default as Footer } from "./Footer";
+
+// when importing them on the App.jsx file, you can write :
+
+// import { serviceData, Nav, Footer } from "./Component/index"
+
+// watched this in a Youtube short video and found value on it...
--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import serviceData from "/Components/serviceData";
 import "./ServicePage.css";
 
-function ServicesPage() {
+function ServicesPage(services) {
   return (
     <div className="service-page-container">
       <h1>Servicios</h1>
@@ -43,7 +44,7 @@ function ServicesPage() {
           <img src="/images/lic-martinez-logo.png" alt="logo" />
           <div className="card-service-text-c">
             <h3 className="card-subheading">SERVICE 1</h3>
-            <p className="card-service-p"> TESTING </p>
+            <p className="card-service-p"> text </p>
             <link rel="stylesheet" href="/contacto" className="href" />
           </div>
         </div>
@@ -51,7 +52,7 @@ function ServicesPage() {
 
       {/* Flex container 3*/}
       <div className="flex-container">
-        {/* Third card container */}
+        {/* Quinto card container */}
         <div className="card-flex-container">
           <img src="/images/lic-martinez-logo.png" alt="logo" />
           <div className="card-service-text-c">
@@ -60,7 +61,51 @@ function ServicesPage() {
             <link rel="stylesheet" href="/contacto" className="href" />
           </div>
         </div>
-        {/* Fourth Card Container */}
+        {/* Sexto Card Container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+      </div>
+
+      {/* Flex container 4*/}
+      <div className="flex-container">
+        {/* Setimo card container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+        {/* Octavo Card Container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+      </div>
+
+      {/* Flex container 5*/}
+      <div className="flex-container">
+        {/* Ninth card container */}
+        <div className="card-flex-container">
+          <img src="/images/lic-martinez-logo.png" alt="logo" />
+          <div className="card-service-text-c">
+            <h3 className="card-subheading">SERVICE 1</h3>
+            <p className="card-service-p"> TESTING </p>
+            <link rel="stylesheet" href="/contacto" className="href" />
+          </div>
+        </div>
+        {/* Tenth Container */}
         <div className="card-flex-container">
           <img src="/images/lic-martinez-logo.png" alt="logo" />
           <div className="card-service-text-c">
