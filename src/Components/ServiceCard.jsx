a17e80f1c699190c97d0980e55aa08109e5be446
carlitrosgp14 <carlosegp.2496@gmail.com>
2026-01-13 16:59:26 -0600

    ./ServicePage&ServiceCard props
    small changes/props instead of hardcoded values

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./ServiceCard.css";
 
-function ServiceCard({ img, name, description }) {
+function ServiceCard(props) {
   return (
     <div className="card-flex-container">
-      <img src={img || "/images/default.jpg"} alt={name} />
+      <img src={props.img || "/images/default.jpg"} alt={props.name} />
 
       <div className="card-service-text-c">
-        <h3 className="card-subheading">{name}</h3>
-        <p className="card-service-p">{description}</p>
+        <h3 className="card-subheading">{props.name}</h3>
+        <p className="card-service-p">{props.description}</p>
 
         <a href="/contacto" className="href">
           Contáctanos
